93c9556e92b29c6db0cabaa73a4402638f7bb981
geosc2506 <geovanniballeto@gmail.com>
2025-06-03 17:07:24 -0500

    Movido index.html a templates

--- app.py
+++ app.py
@@ -4,7 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template("index.html")
